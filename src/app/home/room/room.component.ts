7ec0f8a711397b02f92c3be894eeb6bc84556db5
Roberth Loaiza <pauloaiza@hotmail.es>
2018-04-23 14:10:39 +0200

    Refactory Login

--- src/app/home/room/room.component.ts
+++ src/app/home/room/room.component.ts
@@ -5,6 +5,8 @@ import {ReserveComponent} from '../reserve/reserve.component';
 import {RoomService} from '../shared/room.service';
 import {Room} from '../shared/room.model';
 import {RoomDataService} from '../shared/roomData.service';
+import {MatDialog} from '@angular/material';
+import {LoginComponent} from '../login-dialog.component';
 
 @Component({
   templateUrl: 'room.component.html',
@@ -14,11 +16,9 @@ import {RoomDataService} from '../shared/roomData.service';
 export class RoomComponent implements OnDestroy {
 
   static URL = 'room';
-  reserve: boolean;
-
   data: Room[];
 
-  constructor(private router: Router, private roomService: RoomService, private roomDataService: RoomDataService) {
+  constructor(private router: Router, private roomService: RoomService, private roomDataService: RoomDataService, public loginDialog: MatDialog) {
     this.synchronize();
   }
 
@@ -27,8 +27,15 @@ export class RoomComponent implements OnDestroy {
   }
 
   reservate(room: Room) {
-    this.roomDataService.changeRoom(room);
-    this.router.navigate([HomeComponent.URL, ReserveComponent.URL]);
+    const token: string = localStorage.getItem('tokenHPH');
+    if (token) {
+      this.roomDataService.changeRoom(room);
+      this.router.navigate([HomeComponent.URL, ReserveComponent.URL]);
+    } else {
+      const dialogRef = this.loginDialog.open(LoginComponent, {
+        width: '250px'
+      });
+    }
   }
 
   ngOnDestroy(): void {
